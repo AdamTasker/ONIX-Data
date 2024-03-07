b15677ddd8a4cfa3fd121f9cb6a6301ff7f40752
Adam Tasker <adam@equipmyschool.com>
2024-03-07 21:11:58 +0000

    Added TaxableAmount and TaxAmount to Version3 OnixPriceTax

--- OnixData.Standard/Version3/Price/OnixPriceTax.cs
+++ OnixData.Standard/Version3/Price/OnixPriceTax.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace OnixData.Version3.Price
 {
@@ -76,7 +78,42 @@ namespace OnixData.Version3.Price
                 this.taxRatePercentField = value;
             }
         }
-        
+
+
+        /// <summary>
+        /// The amount of the unit price of the product, excluding tax, which is taxable at the rate specified in an occurrence of the <see cref="OnixPriceTax"/> composite.
+        /// Optional and non-repeating; but required if tax is charged on part of the price.
+        /// Omission of this element implies that tax is charged on the full amount of the price.
+        /// </summary>
+        [XmlChoiceIdentifier("TaxableAmountChoice")]
+        [XmlElement("TaxableAmount")]
+        [XmlElement("x473")]
+        public decimal? TaxableAmount { get; set; }
+        [XmlType(IncludeInSchema = false)]
+        public enum TaxableAmountEnum { TaxableAmount, x473 }
+        [XmlIgnore, DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public TaxableAmountEnum TaxableAmountChoice
+        {
+          get { return SerializationSettings.UseShortTags ? TaxableAmountEnum.x473 : TaxableAmountEnum.TaxableAmount; }
+          set { }
+        }
+        /// <summary>
+        /// The amount of tax chargeable at the rate specified in an occurrence of the <see cref="OnixPriceTax"/> composite.
+        /// Optional and non-repeating; but either <see cref="TaxRatePercent"/> or <see cref="TaxAmount"/> or both must be present in each occurrence of the <see cref="OnixPriceTax"/> composite.
+        /// </summary>
+        [XmlChoiceIdentifier("TaxAmountChoice")]
+        [XmlElement("TaxAmount")]
+        [XmlElement("x474")]
+        public decimal? TaxAmount { get; set; }
+        [XmlType(IncludeInSchema = false)]
+        public enum TaxAmountEnum { TaxAmount, x474 }
+        [XmlIgnore, DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public TaxAmountEnum TaxAmountChoice
+        {
+          get { return SerializationSettings.UseShortTags ? TaxAmountEnum.x474 : TaxAmountEnum.TaxAmount; }
+          set { }
+        }
+
         /// <remarks/>
         public string x472
         {
